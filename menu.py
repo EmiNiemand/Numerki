2b87278ac0ea01bdcbba48d48c075ec6b54ffb12
EmiNiemand <Szymon_PL@live.com>
2022-03-09 15:40:26 +0100

    Drawing function and changes

--- menu.py
+++ menu.py
@@ -1,14 +1,14 @@
 import math
-
+import funkcje_nieliniowe as fn
 
 def start():
     criterion = choose_criterion()
     function = choose_function()
-    upper_range, lower_range = choose_range(function)
+    lower_range, upper_range = choose_range(function)
 
     if criterion == 1:
-        return criterion, function, upper_range, lower_range, float(input("epsilon: "))
-    return criterion, function, upper_range, lower_range, float(input("Liczba iteracji: "))
+        return criterion, function, lower_range, upper_range, float(input("epsilon: "))
+    return criterion, function, lower_range, upper_range, float(input("Liczba iteracji: "))
 
 
 def choose_criterion():
@@ -31,8 +31,12 @@ def choose_function():
               "\n3=2**x-2")
         a = float(input("funkcja: "))
 
-        if a == 1 or a == 2 or a == 3:
-            return a
+        if a == 1:
+            return fn.polynomial
+        if a == 2:
+            return fn.trigonometric
+        if a == 3:
+            return fn.exponential
         print("Wybierz prawidłową liczbę (1 lub 2 lub 3)")
 
 
@@ -42,7 +46,7 @@ def choose_range(function):
         lower_range = float(input("dolny przedział: "))
         upper_range = float(input("górny przedział: "))
         if lower_range < 0 < upper_range:
-            if function == 2 and abs(lower_range) + upper_range > math.pi:
+            if function == fn.trigonometric and abs(lower_range) + upper_range > math.pi:
                 continue
             return lower_range, upper_range
         print("Wybierz prawidłowy przedział (dolny przedział < 0 i górny przedział > 0)")
