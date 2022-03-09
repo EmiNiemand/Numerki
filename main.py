5a454567424df6dce606e22af678f04bba36ade1
EmiNiemand <Szymon_PL@live.com>
2022-03-09 15:30:33 +0100

    Revert "Drawing diagrams + minor changes"
    This reverts commit 052cf76cdb799633338ec35c27ac0cc9bf38bce2.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import menu as men
 
 def main():
     options = []
-    # criterion, function, lower_range, upper_range, epsilon_or_iterations
+    # criterion, function, upper_range, lower_range, epsilon_or_iterations
     options = men.start()
     print(options)
 
