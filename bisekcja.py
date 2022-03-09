42e5fef4c0a697310663e0f9208dc1c868730d10
bortok1 <xbortokx@gmail.com>
2022-03-09 17:37:49 +0100

    accuracy formula change enabled in bisection

--- bisekcja.py
+++ bisekcja.py
@@ -1,20 +1,22 @@
-def bisection(fun, lower_range, upper_range, epsilon):
+def bisection(fun, acc_formula, lower_range, upper_range, epsilon):
     lower = lower_range
     upper = upper_range
-    middle = 0
-
-    accuracy = 0.00001
+    middle = (lower + upper) / 2
 
     for _ in iter(int, 1):
-        middle = (lower + upper) / 2
-        if abs(lower - upper) > epsilon:
-            return middle
-        if abs(fun(middle)) < accuracy:
+        if acc_formula(lower, upper, acc_formula) > epsilon:
             return middle
         if fun(lower) * fun(middle) > 0:
             lower = middle
         else:
             upper = middle
-    return middle
+        middle = (lower + upper) / 2
 
+
+def bisection_A(lower, upper, fun):
+    return abs(upper - lower)
+
+
+def bisection_B(lower, upper, fun):
+    return abs(fun(upper))
 
